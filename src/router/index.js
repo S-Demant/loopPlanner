51e72c3854d3709aa032d8a318283e119d5b537f
Sebastian <sec001@edu.zealand.dk>
2024-09-25 19:51:33 +0200

    Init

---
+++ src/router/index.js
@@ -0,0 +1,27 @@
+import {createRouter, createWebHistory} from 'vue-router';
+import LoopPlanner from "@/views/LoopPlanner.vue";
+import Add from "@/views/Add.vue";
+import Edit from "@/views/Edit.vue";
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+      {
+          path: "/",
+          name: "loop",
+          component: LoopPlanner
+      },
+      {
+          path: "/add",
+          name: "add",
+          component: Add
+      },
+      {
+          path: "/edit",
+          name: "edit",
+          component: Edit
+      }
+  ]
+})
+
+export default router;
---
+++ src/views/LoopPlanner.vue
@@ -0,0 +1,16 @@
+<script setup>
+import Navbar from "../components/Navbar.vue";
+import Card from "@/components/Card.vue";
+</script>
+
+<template>
+    <Navbar />
+    <div class="container">
+        <h1>Hej</h1>
+        <div class="row row-cols-md-3 my-5">
+            <Card :img="'/images/vue.png'" :title="'Lorem ipsum'" :text="'Lorem ipsum...'"/>
+            <Card :img="'/images/vue.png'" :title="'Lorem ipsum'" :text="'Lorem ipsum...'"/>
+            <Card :img="'/images/vue.png'" :title="'Lorem ipsum'" :text="'Lorem ipsum...'"/>
+        </div>
+    </div>
+</template>
